94197ca7017519b87023ad0530a8ac135543c4c5
BeatrizMontoya06 <allputrp8@gmail.com>
2026-08-20 10:56:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('Transcribe tus libros con nuestro sistema de transcripciòn gratuito', 'hola')
+st.write('Transcribe tus libros con nuestro sistema de transcripcion gratuito', 
+         'hola')
            
 st.markdown(f"Quieres escucharlo?, copia el texto")
 text = st.text_area("Ingrese El texto a escuchar.")
