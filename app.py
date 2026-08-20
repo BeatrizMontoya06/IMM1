d2fae78b9023a380e2a0cc5d15f2127ef1b00124
BeatrizMontoya06 <allputrp8@gmail.com>
2026-08-20 10:49:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("Transcriptor de libros (Texto a voz)")
-image = Image.open('gato_raton.png')
+image = Image.open('images.jpeg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
